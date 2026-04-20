a23a516785736f5524d08db4b43ebd1f4087c6f8
ahmed elselly <emxzaliber@gmail.com>
2026-04-20 18:50:01 +0200

    edited the comment in location.module.ts file

--- src/modules/location/location.module.ts
+++ src/modules/location/location.module.ts
@@ -22,7 +22,8 @@ import { MongoLocationRepository } from "./repositories/mongo-location.repositor
 
 
 	],
-	// when we want to switch to another database, we just need to implement the LocationRepository interface for that database and update the provider here without changing the service or controller logic
+	// when we want to switch to another database, we just need to implement the LocationRepository interface for that database
+	// and update the provider here without changing the service or controller logic
 	// 	providers: [
 	//   {
 	//     provide: 'LocationRepository',
